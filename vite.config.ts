5c93ca6746722ff831946cba040312b71be55034
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-11-02 00:35:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import { createServer } from "./server";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 5021,
+    allowedHosts: true,
     fs: {
       allow: [".", "./client", "./shared"],
       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
